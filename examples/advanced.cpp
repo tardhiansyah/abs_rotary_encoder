2f34aab9d36125eb6d7470a920ed60faf6505621
tardhiansyah <tardhians@gmail.com>
2023-08-19 10:29:55 +0700

    Add advanced example

--- examples/advanced.cpp
+++ examples/advanced.cpp
@@ -1,7 +1,10 @@
 #include <Arduino.h>
+#include <EEPROM.h>
 #include <RotaryEncoder.h>
 
 
+const byte EEPROM_ALLOC_SIZE = 64;
+const byte EEPROM_SAVE_ADDR = 1;
 const byte ENCODER_BIT_RESOLUTION = 10;
 const byte ENCODER_PINS[ENCODER_BIT_RESOLUTION] = {22, 13, 14, 27, 26, 25, 33, 32, 35, 34};
 
@@ -13,7 +16,13 @@ void IRAM_ATTR isr();
 void setup()
 {
     Serial.begin(115200);
-    encoder.init(ENCODER_PINS);
+    EEPROM.begin(EEPROM_ALLOC_SIZE);
+
+    // Get the last number of rotation from EEPROM
+    int nLastRotation = EEPROM.get(EEPROM_SAVE_ADDR, nLastRotation);
+    encoder.setNumberOfRotation(nLastRotation);
+
+    encoder.init(ENCODER_PINS, true);
 
     // Set interrupt to encoder pins
     for (byte i = 0; i < ENCODER_BIT_RESOLUTION; i++)
@@ -24,6 +33,15 @@ void setup()
 
 void loop()
 {
+    // Save the number of rotation to EEPROM
+    static int nRotation;
+    if (nRotation != encoder.getNumberOfRotation())
+    {
+        nRotation = encoder.getNumberOfRotation();
+        EEPROM.put(EEPROM_SAVE_ADDR, nRotation);
+        EEPROM.commit();
+    }
+
     // Get the angle reading
     float angle = encoder.getDegree();
 
@@ -38,7 +56,7 @@ void loop()
 
     // Check if the encoder on zero position
     bool isOnZero = encoder.isZero();
-
+    
     // Always call this on loop
     encoder.loop();
 }
